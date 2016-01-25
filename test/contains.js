e8c5d56bfdf6aa0079c43395970088ae863b55ac
CaffeineFusion <owensmith@live.com.au>
2016-01-25 14:32:54 +1100

    fixed EllipseContains bug, expanded Ellipse testing

--- test/contains.js
+++ test/contains.js
@@ -14,34 +14,85 @@ var ellipseWrapper = function(point, ellipse) {
 	return EllipseContains(point, {x:ellipse.x, y:ellipse.y}, ellipse.w, ellipse.h);
 }
 
+
+/**
+* testGenerator takes an ellipse and determines the necessary testing points basid on the values passed in.
+**/
+var ellipseTestGenerator = function (e) {
+
+	it('Point on left-most X', function () {
+        assert.equal(ellipseWrapper({x:e.x-(e.w/2), y:e.y}, e),true, 'point on left-most X boundary should return true');
+    });
+
+	it('Point outside left-most X', function () {
+        assert.equal(ellipseWrapper({x:e.x-(e.w/2)-1, y:e.y}, e),false, 'point beyond left-most X boundary should return false');
+    });
+
+	it('Point on top Y', function () {
+       assert.equal(ellipseWrapper({x:e.x, y:e.y-(e.h/2)}, e),true, 'point on top-most Y point should return true');
+    });
+
+	it('Point outside top Y', function () {
+       assert.equal(ellipseWrapper({x:e.x, y:e.y-(e.h/2)-1}, e),false, 'point above top-most Y point should return false');
+    });
+
+	it('Point on right-most X', function () {
+        assert.equal(ellipseWrapper({x:e.x+(e.w/2), y:e.y}, e),true, 'point on right-most X point should return true');
+    });
+
+	it('Point outside right-most X', function () {
+        assert.equal(ellipseWrapper({x:e.x+(e.w/2)+1, y:e.y}, e),false, 'point beyond right-most X point should return false');
+    });
+
+	it('Point on bottom-most Y', function () {
+        assert.equal(ellipseWrapper({x:e.x, y:e.y+(e.h/2)}, e),true, 'point on bottom-most Y point should return true');
+    });
+
+	it('Point outside bottom-most Y', function () {
+        assert.equal(ellipseWrapper({x:e.x, y:e.y+(e.h/2)+1}, e),false, 'point beyond bottom-most Y point should return false');
+    });
+
+	it('Point on center-point', function () {
+        assert.equal(ellipseWrapper({x:e.x, y:e.y}, e),true, 'point on center-point should return true');
+    });
+
+};
+
 describe('Contains', function () {
 	describe('#EllipseContains()', function () {
 		describe('Ellipse on X boundary', function() {
 			var e = {x:10, y:10, w:20, h:5};
-
-			it('Point on X boundary', function () {
-	            assert.equal(ellipseWrapper({x:0, y:10}, e),true, 'point on X boundary should return true');
-	        });
-
-			it('Point on top Y', function () {
-	            assert.equal(ellipseWrapper({x:10, y:5}, e),true, 'point on top-most Y point should return true');
-	        });
-
-			it('Point on right-most X', function () {
-	            assert.equal(ellipseWrapper({x:20, y:10}, e),true, 'point on right-most X point should return true');
-	        });
-
-			it('Point on bottom-most Y', function () {
-	            assert.equal(ellipseWrapper({x:10, y:15}, e),true, 'point on bottom-most Y point should return true');
-	        });
-
-			it('Point on center-point', function () {
-	            assert.equal(ellipseWrapper({x:10, y:10}, e),true, 'point on center-point should return true');
-	        });
-
+			ellipseTestGenerator(e);
 		});
-		describe('Ellipse on Y boundary', function() {
 
+		describe('Ellipse on Y boundary', function() {
+			var e = {x:10, y:10, w:2, h:20};
+			ellipseTestGenerator(e);
+		});
+
+		describe('Ellipse on X and Y boundary', function() {
+			var e = {x:10, y:10, w:20, h:20};
+			ellipseTestGenerator(e);
+		});
+
+		describe('Tiny Ellipse', function() {
+			var e = {x:10, y:10, w:2, h:4};
+			ellipseTestGenerator(e);
+		});
+
+		describe('Large Ellipse', function() {
+			var e = {x:100, y:100, w:200, h:200};
+			ellipseTestGenerator(e);
+		});
+
+		describe('Overhanging X boundary', function() {
+			var e = {x:5, y:100, w:20, h:20};
+			ellipseTestGenerator(e);
+		});
+
+		describe('Overhanging Y boundary', function() {
+			var e = {x:100, y:5, w:20, h:20};
+			ellipseTestGenerator(e);
 		});
 	});
 });
